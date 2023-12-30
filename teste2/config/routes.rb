b03407166a57beaefc88fb681bcea1d0efac6f1b
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-12-30 20:42:12 -0300

    chore: implementação controller User, para visualizacao dos dados via JSON

--- teste2/config/routes.rb
+++ teste2/config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  get 'divisible_numbers/find_divisible_numbers'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  Rails.application.routes.draw do
-    get 'divisible_numbers/find_divisible_numbers', to: 'divisible_numbers#find_divisible_numbers'
-  end
+  get 'users/index'
+  get 'up' => 'rails/health#show', as: :rails_health_check
+  get 'divisible_numbers/find_divisible_numbers', to: 'divisible_numbers#find_divisible_numbers'
+  resources :users, only: [:index]
 end
