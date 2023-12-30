b3aa32085d6d33067e620d38c7db442072f247e3
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-12-30 18:56:22 -0300

    chore: configurando rotas para tarefa 1

--- teste2/config/routes.rb
+++ teste2/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'divisible_numbers/find_divisible_numbers'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +8,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  Rails.application.routes.draw do
+    get 'divisible_numbers/find_divisible_numbers', to: 'divisible_numbers#find_divisible_numbers'
+  end
 end
